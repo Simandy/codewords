80e4923b0c2562a9416f05ad1ab90b7323d01984
karenanndonnachie <karenanndonnachie@gmail.com>
2020-08-13 16:00:30 +1000

    Add files via upload

--- processing/CODEWORDS13aug/CODEWORDS13aug.js
+++ processing/CODEWORDS13aug/CODEWORDS13aug.js
@@ -1,10 +1,11 @@
 //codewords workshop Thursaday 13 August
 //Code by karen ann donnachie
+// typeface 
 
 function preload() {
   inconsolata = loadFont('data/inconsolata.otf');
   DIN = loadFont('data/DIN.otf');
-  coldera=loadFont('data/Coldera.otf');
+  coldera=loadFont('data/coldera.otf');
 }
 var spin=0;
 var letterSize=24;
@@ -22,11 +23,10 @@ function draw() {
   translate(300,300);
   rotate(spin);
   textSize(letterSize);
-  textFont(coldera);
+  textFont(inconsolata);
   text ('THIS TEXT IS SPINNING', 0,0);
   spin+=map(mouseX,0,600,-3,3); //this will make the mouseX affect the spin direction and speed
   letterSize=map(mouseY,0,600,8,72);
   stroke(0);
-  pop();
   background(240,30);
 }
